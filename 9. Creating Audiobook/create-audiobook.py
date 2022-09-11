7deffdc10effb9ba40d83b5c061899f8a9dee919
Jarrian <glorianagojar@gmail.com>
2022-09-11 16:01:06 +0800

    update

--- 9. Creating Audiobook/create-audiobook.py
+++ 9. Creating Audiobook/create-audiobook.py
@@ -8,7 +8,9 @@ audio_dir = "audio/"
 engine = pyttsx3.init()
 voices = engine.getProperty("voices")
 engine.setProperty("voice", voices[1].id)
+
 PDF_Reader = PyPDF2.PdfFileReader(open(f"{pdf_dir}sample.pdf", "rb"))
+
 for i in range(PDF_Reader.numPages):
     Text = PDF_Reader.getPage(i).extractText()
     engine.save_to_file(Text, f"{audio_dir}audio{i}.mp3")
