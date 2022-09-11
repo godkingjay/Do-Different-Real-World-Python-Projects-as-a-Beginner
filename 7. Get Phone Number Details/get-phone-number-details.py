b6eb2f6b0d6d6ff8faae8921e3e0ca56a0d299ee
Jarrian <glorianagojar@gmail.com>
2022-09-11 11:05:53 +0800

    update

--- 7. Get Phone Number Details/get-phone-number-details.py
+++ 7. Get Phone Number Details/get-phone-number-details.py
@@ -4,12 +4,23 @@ from phonenumbers import geocoder
 from phonenumbers import carrier
 from phonenumbers import timezone
 
-number = input("Phone Number: ")
-number = ph.parse(number)
-country = geocoder.description_for_number(number, "en")
+number = 0
+country = ""
+
+while not len(country):
+    number = input("Input the number: ")
+    if number[0] != '+' or len(number) < 4 or len(number) > 15:
+        print("Please input a valid Phone Number!")
+        continue;
+    number = ph.parse(number)
+    country = geocoder.description_for_number(number, "en")
+
 sim_card_provider = carrier.name_for_number(number, "en")
 time_zone = timezone.time_zones_for_number(number)
 
+print("\n")
+
+print(number)
 print(f"Country: {country}")
 print(f"Sim Card Provider: {sim_card_provider}")
 print(f"Time Zone: {time_zone}")
