ee7dc6d46c8bfa149eedb957a0c4671fac5290f8
Jarrian <glorianagojar@gmail.com>
2022-09-11 16:34:05 +0800

    update

--- 10. Coverting Video to Audio/video-to-audio-converter.py
+++ 10. Coverting Video to Audio/video-to-audio-converter.py
@@ -6,3 +6,4 @@ video_dir = "video/"
 filename = "mr-blue-sky-electric-light-orchestra-lyrics"
 
 clip = me.VideoFileClip(f"{video_dir}{filename}.mp4");
+clip.audio.write_audiofile(f"{audio_dir}{filename}.mp3")
