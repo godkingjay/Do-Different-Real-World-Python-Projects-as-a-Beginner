60735da45ee20323f3355097da67610ca255df99
Jarrian <glorianagojar@gmail.com>
2022-09-12 14:14:17 +0800

    update

--- 12. Taking Screenshot/taking-screenshot.py
+++ 12. Taking Screenshot/taking-screenshot.py
@@ -1,2 +1,14 @@
 # import Python Modules
+from os.path import exists
 import pyscreenshot
+
+screenshot_dir = "screenshots/"
+screenshot_name = "screenshot"
+screenshot_ext = ".jpg"
+n = 0
+screenshot = pyscreenshot.grab()
+
+while exists(f"{screenshot_dir}{screenshot_name}{n}{screenshot_ext}"):
+    n+=1
+
+screenshot.save(f"{screenshot_dir}{screenshot_name}{n}{screenshot_ext}")
