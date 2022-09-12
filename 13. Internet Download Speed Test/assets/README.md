caae9a5795773ceaca9bc3fac3769489e121e8cc
Jarrian <glorianagojar@gmail.com>
2022-09-12 14:56:11 +0800

    update

--- 13. Internet Download Speed Test/assets/README.md
+++ 13. Internet Download Speed Test/assets/README.md
@@ -3,3 +3,4 @@
 ***
 
 ***internetspeed_102161.ico*** is downloaded from [here](https://icon-icons.com/icon/internet-speed-speedometer-test/102161).
+***internet-speed_icon-icons.com_52832.png*** is downloaded from [here](https://icon-icons.com/icon/internet-speed/52832).
--- 13. Internet Download Speed Test/internet-download-speed-test.py
+++ 13. Internet Download Speed Test/internet-download-speed-test.py
@@ -1,8 +1,10 @@
 # import Python Modules
 import tkinter as tk
+from PIL import Image, ImageTk
 
 assets_dir = "assets/"
 icon_file = "internetspeed_102161.ico"
+logo_file = "internet-speed_icon-icons.com_52832.png"
 
 root = tk.Tk()
 
@@ -10,5 +12,18 @@ root.geometry("450x600")
 root.resizable(0, 0)
 root.title("Internet Download Speed Test")
 root.iconbitmap(f"{assets_dir}{icon_file}")
+
+logo = Image.open(f"{assets_dir}{logo_file}")
+logo = logo.resize((100, 100), Image.ANTIALIAS)
+logo = ImageTk.PhotoImage(logo)
+logo_label = tk.Label(image=logo)
+logo_label.image = logo
+logo_label.pack(padx=20, pady=10)
+
+text_label = tk.Label(root, text="Internet Download Speed Test", font=("Arial", 18, "bold"), fg="green")
+text_label.pack(padx=0, pady=10)
+
+button1 = tk.Button(root, text="Test", font=("Arial", 18, "bold"))
+button1.pack()
 
 root.mainloop()
